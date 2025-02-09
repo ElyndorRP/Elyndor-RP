ea072b0656a310442d376aa804beafdfad8333eb
ElyndorRP <clement98278@gmail.com>
2025-02-09 16:46:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// Script to smoothly scroll to sections
-function scrollToSection(sectionId) {
-    const element = document.getElementById(sectionId);
-    window.scrollTo({
-      top: element.offsetTop,
-      behavior: 'smooth'
-    });
-  }
-  
+
